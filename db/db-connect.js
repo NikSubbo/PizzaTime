09c3ed7bf597af0afadc5502eaeadce1540dbb5c
ns <ns@nsubbotin2.nb.ipa.dataart.net>
2026-06-16 22:20:25 +0300

    seed

--- db/db-connect.js
+++ db/db-connect.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+const seed = require("./seed");
 
 mongoose
   .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/pizzatime", {
@@ -8,7 +9,10 @@ mongoose
     useUnifiedTopology: true,
     useFindAndModify: false,
   })
-  .then(() => console.log("Connected to the Database"))
+  .then(async () => {
+    console.log("Connected to the Database");
+    await seed();
+  })
   .catch((err) => console.log(err));
 
 module.exports = mongoose.connection;
--- db/seed.js
+++ db/seed.js
@@ -1,52 +1,56 @@
 const dbConnection = require("./db-connect");
 
-const Pizza = require('./models/pizzas')
+const Pizza = require("./models/pizzas");
 
 const titles = [
-  'Pepperoni',
-  'Margherita',
-  'Cheese',
-  'Mexican',
-  'Hawaiian',
-  'Ham & mushroom',
-  'Seafood',
-  'Vegetarian'
+  "Pepperoni",
+  "Margherita",
+  "Cheese",
+  "Mexican",
+  "Hawaiian",
+  "Ham & mushroom",
+  "Seafood",
+  "Vegetarian",
 ];
 
 const descriptions = [
-  'American classic with spicy pepperoni, Mozzarella and signature tomato sauce',
-  'Traditional recipe with signature tomato sauce, Mozzarella, oregano and juicy tomatoes',
-  'Simple and delicious cheese pizza with signature tomato sauce and trailing Mozzarella',
-  'Spicy pizza with chicken fillet, tomato sauce, Mozzarella, mushrooms, onions, tomatoes, sweet green and jalapeno peppers',
-  'Tropical classic with flavorful ham, tomato sauce and Mozzarella combined with pineapple bits',
-  'Delicate pizza with Garlic ranch sauce, Mozzarella, mushrooms, flavorful ham and garlic',
-  'Sea cocktail, garlic ranch sauce, Mozzarella, sweet green and red peppers, onions, garlic and Italian herbs blend',
-  'Mozzarella cheese, tomato sauce, tomatoes, mushrooms, sweet green pepper, onion, olives'
+  "American classic with spicy pepperoni, Mozzarella and signature tomato sauce",
+  "Traditional recipe with signature tomato sauce, Mozzarella, oregano and juicy tomatoes",
+  "Simple and delicious cheese pizza with signature tomato sauce and trailing Mozzarella",
+  "Spicy pizza with chicken fillet, tomato sauce, Mozzarella, mushrooms, onions, tomatoes, sweet green and jalapeno peppers",
+  "Tropical classic with flavorful ham, tomato sauce and Mozzarella combined with pineapple bits",
+  "Delicate pizza with Garlic ranch sauce, Mozzarella, mushrooms, flavorful ham and garlic",
+  "Sea cocktail, garlic ranch sauce, Mozzarella, sweet green and red peppers, onions, garlic and Italian herbs blend",
+  "Mozzarella cheese, tomato sauce, tomatoes, mushrooms, sweet green pepper, onion, olives",
 ];
 
 const pictures = [
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592062007/Pizzas/Pepper_xzqauq.jpg',
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Margarita_bzgjjd.jpg',
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Cheese_dc6rth.jpg',
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592056319/Pizzas/Mexico_ppwzjg.jpg',
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Hawaii_wz5aau.jpg',
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Hum_Mushrooms_olnhb1.jpg',
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Seafood_uodjmk.jpg',
-  'https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Vegan_orj8qj.jpg'
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592062007/Pizzas/Pepper_xzqauq.jpg",
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Margarita_bzgjjd.jpg",
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Cheese_dc6rth.jpg",
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592056319/Pizzas/Mexico_ppwzjg.jpg",
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Hawaii_wz5aau.jpg",
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Hum_Mushrooms_olnhb1.jpg",
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Seafood_uodjmk.jpg",
+  "https://res.cloudinary.com/dt0icdja8/image/upload/v1592056320/Pizzas/Vegan_orj8qj.jpg",
 ];
 
 const prices = [9, 8, 7, 10, 9, 9, 11, 8];
 
 async function seed() {
-  for (let i = 0; i < 8; i++) {
-    let pizzas = new Pizza({
-      title: titles[i],
-      description: descriptions[i],
-      picture: pictures[i],
-      price: prices[i]
-    });
-    await pizzas.save();
+  const count = await Pizza.countDocuments();
+
+  if (count === 0) {
+    for (let i = 0; i < 8; i++) {
+      let pizzas = new Pizza({
+        title: titles[i],
+        description: descriptions[i],
+        picture: pictures[i],
+        price: prices[i],
+      });
+      await pizzas.save();
+    }
   }
 }
 
-// seed()
+module.exports = seed;
